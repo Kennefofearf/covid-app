b3e5efdfebd827f06eaa5600883e090b1e6cc0de
Kennefofearf <kpeterson344@gmail.com>
2023-03-27 12:40:06 -0400

    Fixed data fetching error and added a chevron icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './navbar.module.css'
 import { useFetchData } from "../hooks/fetchData";
 
 export default function Navbar(props) {
   const {selected, setSelected} = props
-  const countriesUrl = 'https://covid-api.mmediagroup.fr/v1/cases'
+  const countriesUrl = 'https://covid-api.com/api/regions?per_page=20'
   const { data, loading, error } = useFetchData(countriesUrl)
+
+  const [open, setOpen] = useState(false)
 
   if (error) {
     return <div>
-      YOU DID SOMETHING WRONG
+      ERROR
     </div>
   }
 
   return (
-    <div className='styles.navbar'>
+    <div className={styles.navbar} onClick={() => setOpen(!open)}>
       {loading ?
       <div>Loading</div> :
       <div>{selected}</div>
       }
+      <i style={{transform: `${open ? 'rotate(180deg)' : ''}`, transitionDuration: '300ms'}} className="fa-solid fa-chevron-down"></i>
     </div>
   )
 }
