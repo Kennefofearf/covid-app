465ccee470f87f5302d72574e5aa4396668cf07b
Kennefofearf <kpeterson344@gmail.com>
2023-03-29 22:34:24 -0400

    Corrected the date calculations

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,17 +5,18 @@ export default function Main(props) {
     const { selected } = props
     const url = 'https://covid-api.com/api/reports/total?date=2020-03-14&iiso=USA'
     const selectedRegion = {selected}
-    // const date = new Date()
-    // let year = date.getFullYear()
-    // let month = date.getMonth() + 1
-    // let day = date.getDate()
-    // const new_Url = `https://covid-api.com/api/reports/total?date=${year}-${month}-${day}&iso=${selectedRegion}`
+    const date = new Date()
+    let year = date.getFullYear()
+    let month = ('0' + (date.getMonth() + 1)).slice(-2)
+    let day = ('0' + (date.getDate())).slice(-2)
+    const new_Url = `https://covid-api.com/api/reports/total?date=${year}-${month}-${day}&iso=${selectedRegion}`
+    console.log(new_Url)
     // const { data, loading, error } = useFetchData(new_Url)
     // const deaths = data.data.deaths
     // const cases = data.data.confirmed
 
     if (selected === 'Global') {
-        // selectedRegion = 'USA'
+        //selectedRegion = 'USA'
         return <div>
             Globobble
         </div>
