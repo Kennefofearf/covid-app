6b6bdf6027d67d9c70cf55d3ad6aab5026cd6f24
Kennefofearf <kpeterson344@gmail.com>
2023-03-30 23:03:31 -0400

    Added a conditional statement to account for Feb

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,16 +9,19 @@ export default function Main(props) {
     const selectedRegion = iso
     const date = new Date()
     let year = date.getFullYear()
-    let month = ('0' + (date.getMonth() + 1)).slice(-2)
-    let day = ('0' + (date.getDate())).slice(-2)
+    let month = ('0' + (date.getMonth() + 1 - 1)).slice(-2)
+    var day = ('0' + (date.getDate())).slice(-2)
+    if (month == '02' && day > 28) {
+        day = ('0' + (date.getDate() - 2)).slice(-2)
+    }
     const new_Url = `https://covid-api.com/api/reports/total?date=${year}-${month}-${day}&iso=${selectedRegion}`
-    console.log(new_Url)
-    // const { data, loading, error } = useFetchData(new_Url)
-    // const deaths = data.data.deaths
-    // const cases = data.data.confirmed
+    const { data, loading, error } = useFetchData(new_Url)
+    // const deaths = data.deaths
+    // const cases = data.confirmed
+    console.log(data)
 
     if (selected === 'Global') {
-        // selectedRegion = 'USA'
+        //selectedRegion = 'USA'
         return <div>
             Globobble
         </div>
